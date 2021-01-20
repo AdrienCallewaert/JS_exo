aba5826894ebaa9620f5a30161ed267c962d5934
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-20 17:01:10 +0100

    Update test_4_5.js

---
+++ JS_ex_4.0/test_4_5.js
@@ -0,0 +1,6 @@
+const readlineSync = require("readline-sync");
+
+let xA = new Number(readlineSync.question('Enter a coordinate x for the point A : '));
+let yA = new Number(readlineSync.question('Enter a coordinate y for the point A : '));
+let xB = new Number(readlineSync.question('Enter a coordinate x for the point B : '));
+let yB = new Number(readlineSync.question('Enter a coordinate y for the point B : '));
