47bf07d80c1593269c69e7a48270f9dd7fd87904
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-21 09:14:26 +0100

    up

--- JS_ex_4.0/test_4_5.js
+++ JS_ex_4.0/test_4_5.js
@@ -4,3 +4,13 @@ let xA = new Number(readlineSync.question('Enter a coordinate x for the point A 
 let yA = new Number(readlineSync.question('Enter a coordinate y for the point A : '));
 let xB = new Number(readlineSync.question('Enter a coordinate x for the point B : '));
 let yB = new Number(readlineSync.question('Enter a coordinate y for the point B : '));
+
+function calcDistance (xA, yA, xB, yB){
+    this.xA = xA;
+    this.yA = yA;
+    this.xB = xB;
+    this.yB = yB;
+    let distance = Math.sqrt((Math.pow(this.xB-this.xA,2))+(Math.pow(this.yB-this.yA,2)));
+    return distance;
+}
+console.log(calcDistance(xA, yA, xB, yB));
